792775fd6e06b6c6194b7cf8f0f71637170d5830
sebifp <sebiflorinp@gmail.com>
2024-01-03 15:46:51 +0200

    ADD models library

---
+++ libs/dbInteraction/users/users.h
@@ -0,0 +1,8 @@
+//
+// Created by sebifp on 03.01.2024.
+//
+
+#ifndef CP2_USERS_H
+#define CP2_USERS_H
+
+#endif //CP2_USERS_H
---
+++ libs/models/models.h
@@ -0,0 +1,31 @@
+#ifndef CP2_MODELS_H
+#define CP2_MODELS_H
+    // user model
+    typedef struct {
+       int id;
+       char username[200];
+       char password[200];
+    } User;
+
+    // transaction model
+    typedef struct {
+        int id;
+        int userId;
+        int type;
+        float value;
+        int receiverId;
+        int day;
+        int month;
+        int year;
+    } Transaction;
+
+    // important user model
+    typedef struct {
+        int id;
+        // id of the user to whom is another user important
+        int idUser1;
+        // id of the user who is important
+        int idUser2;
+        char description[1000];
+    } ImportantUser;
+#endif //CP2_MODELS_H
