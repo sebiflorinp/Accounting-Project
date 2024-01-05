b70e71697a35a254f9843c7870080d3602ecc1c4
sebifp <sebiflorinp@gmail.com>
2024-01-05 16:49:02 +0200

    UPDATE file structure in libs

--- libs/accounts/accounts.c
+++ libs/accounts/accounts.c
@@ -1,5 +1,5 @@
 #include "accounts.h"
-#include "../../models/models.h"
+#include "../models/models.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include "libs/dbInteraction/users/users.h"
-#include "libs/dbInteraction/accounts/accounts.h"
+#include "libs/users/users.h"
+#include "libs/accounts/accounts.h"
 #include "libs/transactions/transactions.h"
 #include "libs/reports/reports.h"
 #include <time.h>
 #include "libs/importantUsers/importantUsers.h"
 
 int main() {
-    return 0;
+
 }
